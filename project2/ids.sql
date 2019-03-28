ebc9d2dc0e24cb8ea3bcab19761fefdfd51f1a96
Matej Soroka <hi@matejsoroka.com>
2019-03-28 13:22:42 +0100

    Adding FK between Adventure and Location, adding quantity to equipment

--- project2/ids.sql
+++ project2/ids.sql
@@ -167,22 +167,24 @@ CREATE TABLE Player(
   kills int
 );
 
-create table Adventure(
-	adventure_id int generated as identity constraint PK_adventure primary key,
-	difficulty int,
-	objective varchar(255),
-	pj_id int,
-  CONSTRAINT FK_Adventure FOREIGN KEY (pj_id) REFERENCES Player(player_id)
-);
-
 create table Author(
-	author_id int generated as identity constraint PK_author primary key,
-	name varchar(255)
+  author_id int generated as identity constraint PK_author primary key,
+  name varchar(255)
 );
 
 create table Location(
-	location_id int generated as identity constraint PK_location primary key,
-	name varchar(255)
+  location_id int generated as identity constraint PK_location primary key,
+  name varchar(255)
+);
+
+create table Adventure(
+  adventure_id int generated as identity constraint PK_adventure primary key,
+  difficulty int,
+  objective varchar(255),
+  pj_id int,
+  location_id int,
+  CONSTRAINT FK_Adventure FOREIGN KEY (pj_id) REFERENCES Player(player_id),
+  CONSTRAINT FK_Location FOREIGN KEY (location_id) REFERENCES Location(location_id)
 );
 
 create table adventure_author(
@@ -193,15 +195,15 @@ create table adventure_author(
 );
 
 create table Death(
-	death_id int generated as identity constraint PK_death primary key,
-	"date" int,
-	location_id int,
-	CONSTRAINT FK_Death FOREIGN KEY (location_id) REFERENCES Location(location_id)
+  death_id int generated as identity constraint PK_death primary key,
+  "date" int,
+  location_id int,
+  CONSTRAINT FK_Death FOREIGN KEY (location_id) REFERENCES Location(location_id)
 );
 
 create table Character(
-	character_id int generated as identity constraint PK_character primary key,
-	name varchar(255),
+  character_id int generated as identity constraint PK_character primary key,
+  name varchar(255),
   race varchar(255),
   class varchar(255),
   "level" int,
@@ -219,11 +221,12 @@ create table character_adventure(
 );
 
 create table Equipment(
-	equipment_id int generated as identity constraint PK_equipment primary key,
-	type varchar(255)
+  equipment_id int generated as identity constraint PK_equipment primary key,
+  type varchar(255)
 );
 
 create table character_equipment(
+    quantity int,
     equipment_id int,
     FOREIGN KEY (equipment_id) REFERENCES Equipment(equipment_id),
     character_id int,
@@ -231,19 +234,19 @@ create table character_equipment(
 );
 
 create table Game_element(
-	element_id int generated as identity constraint PK_element primary key,
-	name varchar(255)
+  element_id int generated as identity constraint PK_element primary key,
+  name varchar(255)
 );
 
 create table Map(
   element_id int,
-	scale int,
-	CONSTRAINT PK_Map PRIMARY KEY (element_id),
+  scale int,
+  CONSTRAINT PK_Map PRIMARY KEY (element_id),
   CONSTRAINT FK_Map FOREIGN KEY (element_id) REFERENCES Game_element ON DELETE CASCADE
 );
 
 create table Enemy(
-	race varchar(255),
+  race varchar(255),
   "level" int,
   element_id int,
   CONSTRAINT PK_Enemy PRIMARY KEY (element_id),
@@ -251,9 +254,9 @@ create table Enemy(
 );
 
 create table Campaign(
-	campaign_id int generated as identity constraint PK_campaign primary key,
-	difficulty int,
-	objective varchar(255)
+  campaign_id int generated as identity constraint PK_campaign primary key,
+  difficulty int,
+  objective varchar(255)
 );
 
 create table adventure_campaign(
@@ -264,9 +267,9 @@ create table adventure_campaign(
 );
 
 create table Sessions(
-	session_id int generated as identity constraint PK_session primary key,
-	"date" int,
-	place varchar(255)
+  session_id int generated as identity constraint PK_session primary key,
+  "date" int,
+  place varchar(255)
 );
 
 create table adventure_game_element(
