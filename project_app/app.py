6cae151f45949cc786d51671d1017d389cebae71
Matej Soroka <hi@matejsoroka.com>
2019-04-04 11:13:06 +0200

    project app init

---
+++ project_app/app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template
+import cx_Oracle
+import os
+
+app = Flask(__name__)
+
+db_user = os.environ.get('DBAAS_USER_NAME', 'xsorok02')
+db_password = os.environ.get('DBAAS_USER_PASSWORD', '')
+db_connect = os.environ.get('DBAAS_DEFAULT_CONNECT_DESCRIPTOR', "gort.fit.vutbr.cz:1521/orclpdb.gort.fit.vutbr.cz")
+service_port = port=os.environ.get('PORT', '1521')
+
+@app.route('/')
+def index():
+    connection = cx_Oracle.connect(db_user, db_password, db_connect)
+    cur = connection.cursor()
+    cur.execute("SELECT * FROM PLAYER")
+    players = cur.fetchall()
+    cur.close()
+    connection.close()
+    return render_template("index.html", players=players)
+
+
+if __name__ == '__main__':
+    app.run()
