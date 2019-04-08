dd857166f723c0750a20bd15ffe7e51e55666522
Matej Soroka <hi@matejsoroka.com>
2019-04-08 22:42:52 +0200

    refactor

--- project_app/app.py
+++ project_app/app.py
@@ -1,14 +1,8 @@
-from flask import Flask, render_template, request, url_for, redirect
-from model import PlayerInsertForm, AdventureInsertForm
-import cx_Oracle
-import os
+from flask import Flask, render_template, url_for, redirect
+from model import PlayerInsertForm, AdventureInsertForm, model
 
 app = Flask(__name__)
-
-db_user = os.environ.get('DBAAS_USER_NAME', 'xsorok02')
-db_password = os.environ.get('DBAAS_USER_PASSWORD', 'CJiBvpmS')
-db_connect = os.environ.get('DBAAS_DEFAULT_CONNECT_DESCRIPTOR', "gort.fit.vutbr.cz:1521/orclpdb.gort.fit.vutbr.cz")
-service_port = port = os.environ.get('PORT', '1521')
+model = model.Model()
 
 
 @app.route('/')
@@ -18,120 +12,54 @@ def index():
 
 @app.route('/adventures')
 def adventures():
-    connection = cx_Oracle.connect(db_user, db_password, db_connect)
-    cur = connection.cursor()
-    cur.execute("SELECT * FROM ADVENTURE")
-    fetch = cur.fetchall()
-    cur.close()
-    connection.close()
-    return render_template("adventures.html", adventures=fetch)
+    return render_template("adventures.html", adventures=model.get_table("adventure"))
 
 
 @app.route('/adventure-add', methods=('GET', 'POST'))
 def adventure_add():
-    connection = cx_Oracle.connect(db_user, db_password, db_connect)
-    cur = connection.cursor()
-    cur.execute("SELECT * FROM AUTHOR")
-    authors = cur.fetchall()
     form = AdventureInsertForm.AdventureInsertForm()
-    insert_authors = []
-    for author in authors:
-        insert_authors.append((int(author[0]), author[1]))
-    form.authors.choices = insert_authors
+    #  setting default values for from elements
+    form.authors.choices = model.get_pairs("author")
+    form.pj.choices = model.get_pairs("player")
+    form.location.choices = model.get_pairs("location")
+    form.game_elements.choices = model.get_pairs("game_element")
 
-    cur.execute("SELECT * FROM PLAYER")
-    players = cur.fetchall()
-    insert_players = []
-    for player in players:
-        insert_players.append((int(player[0]), player[1]))
-    form.pj.choices = insert_players
-
-    cur.execute("SELECT * FROM LOCATION")
-    locations = cur.fetchall()
-    insert_location = []
-    for location in locations:
-        insert_location.append((int(location[0]), location[1]))
-    form.location.choices = insert_location
-
-    cur.execute("SELECT * FROM GAME_ELEMENT")
-    game_elements = cur.fetchall()
-    insert_game_element = []
-    for game_element in game_elements:
-        insert_game_element.append((int(game_element[0]), game_element[1]))
-    form.game_elements.choices = insert_game_element
-
-    if form.validate_on_submit():
-        query = "INSERT INTO ADVENTURE (OBJECTIVE, DIFFICULTY, PJ_ID, LOCATION_ID) VALUES ('{}', {}, {}, {})".format(
-            form.objective.data, form.difficulty.data, form.pj.data, form.location.data)
-        cur.execute(query)
-        cur.close()
-        connection.commit()
-        cur = connection.cursor()
-        cur.execute("SELECT adventure_id FROM ADVENTURE ORDER BY ADVENTURE_ID DESC")
-        adventure_id = cur.fetchone()[0]
+    if form.validate_on_submit():  # on form submit
+        adventure_insert = {"objective": form.objective.data, "difficulty": form.difficulty.data, "PJ_ID": form.pj.data,
+                            "location_id": form.location.data}
+        model.insert("adventure", adventure_insert)
+        adventure_id = model.get_last_id("adventure")
         for author in form.authors.data:
-            cur.execute("INSERT INTO ADVENTURE_AUTHOR (ADVENTURE_ID, AUTHOR_ID) VALUES ({}, {})".format(adventure_id,
-                                                                                                        author))
+            model.insert("adventure_author", {"adventure_id": adventure_id, "author_id": author})
         for game_element in form.game_elements.data:
-            cur.execute("INSERT INTO ADVENTURE_GAME_ELEMENT (ADVENTURE_ID, GAME_ELEMENT) VALUES ({}, {})".format(
-                adventure_id, game_element))
-        cur.close()
-        connection.commit()
-        connection.close()
+            model.insert("adventure_game_element", {"adventure_id": adventure_id, "game_element": game_element})
         return redirect(url_for('adventures'))
-    cur.close()
-    connection.commit()
-    connection.close()
     return render_template("adventure_add.html", form=form)
 
 
 @app.route('/delete-adventure/<id>')
 def delete_adventure(id):
-    connection = cx_Oracle.connect(db_user, db_password, db_connect)
-    cur = connection.cursor()
-    cur.execute("DELETE FROM ADVENTURE WHERE adventure_id={}".format(id))
-    cur.close()
-    connection.commit()
-    connection.close()
+    model.cursor.execute("DELETE FROM ADVENTURE WHERE adventure_id={}".format(id))
+    model.db.commit()
     return redirect(url_for('adventures'))
 
 
 @app.route('/players')
 def players():
-    connection = cx_Oracle.connect(db_user, db_password, db_connect)
-    cur = connection.cursor()
-    cur.execute("SELECT * FROM PLAYER")
-    fetch = cur.fetchall()
-    cur.close()
-    connection.close()
-    return render_template("players.html", players=fetch)
+    return render_template("players.html", players=model.get_table("player"))
 
 
 @app.route('/delete-player/<id>')
 def delete_player(id):
-    connection = cx_Oracle.connect(db_user, db_password, db_connect)
-    cur = connection.cursor()
-    cur.execute("DELETE FROM PLAYER WHERE player_id={}".format(id))
-    cur.close()
-    connection.commit()
-    connection.close()
+    model.cursor.execute("DELETE FROM PLAYER WHERE player_id={}".format(id))
+    model.db.commit()
     return redirect(url_for('players'))
 
 
 @app.route('/player/<id>')
 def player(id):
-    connection = cx_Oracle.connect(db_user, db_password, db_connect)
-    cur = connection.cursor()
-    cur.execute("SELECT * FROM PLAYER WHERE player_id={}".format(id))
-    player_data = cur.fetchone()
-    cur.execute("SELECT CHARACTER.CHARACTER_ID, CHARACTER.NAME, COUNT(*) as adventure_count "
-                "FROM CHARACTER, CHARACTER_ADVENTURE "
-                "WHERE CHARACTER.PLAYER_ID = {} AND "
-                "CHARACTER_ADVENTURE.CHARACTER_ID = CHARACTER.CHARACTER_ID "
-                "GROUP BY CHARACTER.CHARACTER_ID, CHARACTER.NAME ORDER BY adventure_count DESC".format(id))
-    character_data = cur.fetchall()
-    cur.close()
-    connection.close()
+    player_data = model.get_row("player", id)
+    character_data = model.get_player_characters(id)
     return render_template("player.html", player=player_data, characters=character_data)
 
 
@@ -139,15 +67,7 @@ def player(id):
 def player_add():
     form = PlayerInsertForm.PlayerInsertForm()
     if form.validate_on_submit():
-        connection = cx_Oracle.connect(db_user, db_password, db_connect)
-        cur = connection.cursor()
-        query = "INSERT INTO PLAYER (NAME, GOLD, KILLS) VALUES ('{}', {}, {})".format(form.username.data,
-                                                                                      form.gold.data,
-                                                                                      form.kills.data)
-        cur.execute(query)
-        cur.close()
-        connection.commit()
-        connection.close()
+        model.insert("player", {"name": form.username.data, "gold": form.gold.data, "kills": form.kills.data})
         return redirect(url_for('players'))
     return render_template("player_add.html", form=form)
 
---
+++ project_app/model/model.py
@@ -0,0 +1,64 @@
+import cx_Oracle
+import os
+
+
+class Model:
+    def __init__(self):
+        db_user = os.environ.get('DBAAS_USER_NAME', 'xsorok02')
+        db_password = os.environ.get('DBAAS_USER_PASSWORD', 'CJiBvpmS')
+        db_connect = os.environ.get('DBAAS_DEFAULT_CONNECT_DESCRIPTOR',
+                                    "gort.fit.vutbr.cz:1521/orclpdb.gort.fit.vutbr.cz")
+        self.db = cx_Oracle.connect(db_user, db_password, db_connect)
+        self.cursor = self.db.cursor()
+
+    def get_table(self, table):
+        """Gets every row in table"""
+        self.cursor.execute("SELECT * FROM {}".format(table))
+        return self.cursor.fetchall()
+
+    def get_pairs(self, table):
+        """Gets every row in table in (id, name) format"""
+        self.cursor.execute("SELECT * FROM {}".format(table))
+        data = self.cursor.fetchall()
+        array = []
+        for item in data:
+            array.append((int(item[0]), item[1]))
+        return array
+
+    def insert(self, table, data):
+        """Inserts row into table where data = {col_name: data, ...}"""
+        query = "INSERT INTO {} ".format(table)
+        cols = values = ""
+        i = 1
+        for key, value in data.items():
+            cols = cols + key
+            if isinstance(value, str):
+                values = values + "'" + value + "'"
+            else:
+                values = values + str(value)
+            if i != len(data):
+                cols = cols + ","
+                values = values + ","
+            i = i + 1
+        query = query + "(" + cols + ") VALUES (" + values + ")"
+        self.cursor.execute(query)
+        self.db.commit()
+
+    def get_last_id(self, table):
+        """Gets last inserted id from table"""
+        self.cursor.execute("SELECT {}_id FROM {} ORDER BY {}_id DESC".format(table, table, table))
+        return self.cursor.fetchone()[0]
+
+    def get_row(self, table, id):
+        """Gets row from table with specific id (doesn't work when table_name != table_name_id)"""
+        self.cursor.execute("SELECT * FROM {} WHERE {}_id={}".format(table, table, id))
+        return self.cursor.fetchone()
+
+    def get_player_characters(self, player_id):
+        self.cursor.execute("SELECT CHARACTER.CHARACTER_ID, CHARACTER.NAME, COUNT(*) as adventure_count "
+                            "FROM CHARACTER, CHARACTER_ADVENTURE "
+                            "WHERE CHARACTER.PLAYER_ID = {} AND "
+                            "CHARACTER_ADVENTURE.CHARACTER_ID = CHARACTER.CHARACTER_ID "
+                            "GROUP BY CHARACTER.CHARACTER_ID, CHARACTER.NAME "
+                            "ORDER BY adventure_count DESC".format(player_id))
+        return self.cursor.fetchall()
